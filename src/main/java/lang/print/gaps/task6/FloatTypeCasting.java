cf04cc7edbb26ca746e0e2f0ec2e68f20ce74a5d
Pavel Ulyanov <ulyanovproff@gmail.com>
2023-09-17 23:53:43 +0500

    add complete

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+
+        String result = String.format("%.0f",numberToBeRounded);
+        System.out.println(result);
     }
 }
